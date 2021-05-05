5ab98a819630221ba1fb4c77ef78aa220b1417c9
zazazaxky-m <78693013+zazazaxky-m@users.noreply.github.com>
2021-05-05 07:12:16 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u walletkalian.workename_bebas_saja -p x --cpu 2
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u BTC:128MaabsdMdwtNrw4o5QVMur9UDug6YpF6.Test#11i0-ff25 -p x -k -a rx/0
 while [ 1 ]; do
 sleep 3
 done
